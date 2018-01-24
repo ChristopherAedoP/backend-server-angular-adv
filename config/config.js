6340bacbfc739a357a3d126e8bbc82bc9a585aac
Christopher Aedo <Christopher.Aedo@sonda.com>
2018-01-24 16:02:08 -0300

    seccion 12 terminada backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,8 @@
-module.exports.SEED = 'este-esun-seed-dificil'
+module.exports.SEED = 'este-esun-seed-dificil';
+
+// Google
+
+module.exports.GOOGLE_CLIENT_ID =
+  '72130390978-01rnoj6sntsf30c2diu40920tqnd6ur6.apps.googleusercontent.com';
+
+module.exports.GOOGLE_SECRET = 'Wkew-wBUJq_riOjIE_MxHnxn';
--- routes/login.js
+++ routes/login.js
@@ -8,57 +8,150 @@ var app = express();
 
 var Usuario = require('../models/usuario');
 
+// =====================================================
+// Autentificacion Normal
+//======================================================
 app.post('/', (req, res) => {
+  var body = req.body;
 
-    var body = req.body;
+  Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'Error obtener usuario',
+        errors: err
+      });
+    }
 
-    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                mensaje: 'Error obtener usuario',
-                errors: err
-            });
-        }
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'Credenciales incorrectas - email',
+        errors: { message: 'Credenciales incorrectas - email' }
+      });
+    }
 
-        if (!usuarioDB) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Credenciales incorrectas - email',
-                errors: { message: 'Credenciales incorrectas - email' }
-            });
-        }
+    if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'Credenciales incorrectas - password',
+        errors: { message: 'Credenciales incorrectas - password' }
+      });
+    }
+    //Crear token.
+    usuarioDB.password = ':)';
+    var token = jwt.sign({ usuario: usuarioDB }, seed, { expiresIn: 14400 });
 
-        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Credenciales incorrectas - password',
-                errors: { message: 'Credenciales incorrectas - password' }
-            });
-        }
-        //Crear token.
-        usuarioDB.password = ':)';
-        var token = jwt.sign({ usuario: usuarioDB }, seed, { expiresIn: 14400 })
-
-        res.status(200).json({
-            ok: true,
-            usuario: usuarioDB,
-            token: token,
-            id: usuarioDB._id
-        });
+    res.status(200).json({
+      ok: true,
+      usuario: usuarioDB,
+      token: token,
+      id: usuarioDB._id
     });
-
-
-
+  });
 });
 
+// =====================================================
+// Autentificacion Google
+//======================================================
 
+var GoogleAuth = require('google-auth-library');
 
+// var auth = new GoogleAuth();
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+app.post('/google', (req, res) => {
+  var token = req.body.token || 'asasas';
+  console.log('token', token);
+  var client = new GoogleAuth.OAuth2Client(
+    GOOGLE_CLIENT_ID,
+    GOOGLE_SECRET,
+    ''
+  );
 
+  client.verifyIdToken({ idToken: token, audience: GOOGLE_CLIENT_ID }, function(
+    err,
+    login
+  ) {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'Error token',
+        errors: err
+      });
+    }
+    var payload = login.getPayload();
+    var userid = payload['sub'];
 
+  //   return res.status(200).json({
+  //     ok: false,
+  //     mensaje: 'Error token',
+  //     payload: payload
+  //   });
+  // });
+  Usuario.findOne({ email: payload.email }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'Error al buscar usuario - login',
+        errors: err
+      });
+    }
 
+    if (usuarioDB) {
+      if (!usuarioDB.google) {
+        return res.status(400).json({
+          ok: false,
+          mensaje: 'Debe de usar su autenticacion normal'
+        });
+      } else {
+        //Crear token.
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, seed, {
+          expiresIn: 14400
+        });
 
+        res.status(200).json({
+          ok: true,
+          usuario: usuarioDB,
+          token: token,
+          id: usuarioDB._id
+        });
+      }
+      // si el usuario no existe por correo
+    } else {
+      var usuarioNuevo = new Usuario();
+      usuarioNuevo.nombre = payload.name;
+      usuarioNuevo.email = payload.email;
+      usuarioNuevo.password = ':)';
+      usuarioNuevo.img = payload.picture;
+      usuarioNuevo.google = true;
 
+      usuarioNuevo.save((err, usuarioDB) => {
+        if (err) {
+          return res.status(500).json({
+            ok: false,
+            mensaje: 'Error al grabar usuario - google',
+            errors: err
+          });
+        }
 
+        //Crear token.
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, seed, {
+          expiresIn: 14400
+        });
+
+        res.status(200).json({
+          ok: true,
+          usuario: usuarioDB,
+          token: token,
+          id: usuarioDB._id
+        });
+      });
+    }
+  });
+  });
+});
 
 module.exports = app;
