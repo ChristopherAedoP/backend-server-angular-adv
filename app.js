2d27983fb4276f7445271996a994c8a63013d2cc
Christopher Alexander Aedo Pizarro <Christopher.aedo.p@gmail.com>
2018-02-28 21:14:25 -0300

    preparando para heroku

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var app = express();
 
 // CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    next();
 });
 
 
@@ -33,12 +33,12 @@ var imagenesRoutes = require('./routes/imagenes');
 
 //conexion a base a datos
 mongoose.connection.openUri(
-  'mongodb://localhost:27017/hospitalDB',
-  (err, res) => {
-    if (err) throw err;
+    process.env.MONGODB || 'mongodb://localhost:27017/hospitalDB',
+    (err, res) => {
+        if (err) throw err;
 
-    console.log('base de datos:  \x1b[32m%s\x1b[0m', 'Online');
-  }
+        console.log('base de datos:  \x1b[32m%s\x1b[0m', 'Online');
+    }
 );
 
 // Server index config
@@ -59,8 +59,8 @@ app.use('/', appRoutes);
 
 // Escuchar peticiones.
 app.listen(3000, () => {
-  console.log(
-    'Express server corriendo en el puerto 3000:  \x1b[32m%s\x1b[0m',
-    'Online'
-  );
+    console.log(
+        'Express server corriendo en el puerto 3000:  \x1b[32m%s\x1b[0m',
+        'Online'
+    );
 });
